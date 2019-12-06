f25efbde1fd4db51a6d8327d25b3e53639e0e87b
EyeTyrant <1414jg@gmail.com>
2019-12-06 10:42:18 -0600

    Controllers and Routing Examples complete.

--- src/main/java/org/launchcode/hellospringtwo/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospringtwo/controllers/HelloController.java
@@ -1,10 +1,11 @@
 package org.launchcode.hellospringtwo.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody // can be placed before the class if every method in the class will have a @ResponseBody
+@RequestMapping("hello") // makes /hello root in path for all methods in the class when @ReauestMapping is placed before the class
 public class HelloController {
 /*
  For every GET request made to the provided path, the controller method will be called.
@@ -12,17 +13,47 @@ public class HelloController {
  If the pathname value is null, then the path used is the index path, or /.
 */
 
-  // Handles request at path /hello
+  // Handles request at path /hello/hello
   @GetMapping("hello")
-  @ResponseBody
   public String hello(){
     return "Hello, Spring!";
   }
 
-  // Handles request at path /goodbye
+  // Handles request at path /hello/goodbye
   @GetMapping("goodbye")
-  @ResponseBody
   public String goodbye(){
     return "Goodbye, Spring!";
   }
+
+  // @RequestMapping can handle more than one method i.e. GET and POST at the same path
+  // @RequestParam Handles request formatted as localhost:8080/hello/helloName?aName=aName
+  @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "helloName")
+  public String helloWithQueryParam(@RequestParam String aName){
+    return "Hello " + aName + "!";
+  }
+
+  // @PathVariable Handles requests formatted as localhost:8080/hello/helloName/name
+  @GetMapping("helloName/{name}")
+  public String helloWithPathParam(@PathVariable String name){
+    return "Hello " +  name + "!";
+  }
+/*
+  Also know that you can redirect a user by removing the @ResponseBody annotation
+  from the controller and returning "redirect:/DESIREDPATH".
+*/
+
+  // lives at /hello/form
+  @GetMapping("form")
+  public String helloForm(){
+    return
+      "<html>" +
+        "<body>" +
+          "<form action='helloName' method='post'>" +  // submit a request to /hello/helloName method
+            "<input type='text' name='aName'>" +  // 'aName' is the argument that matches the required parameter passed to helloName method
+            "<input type='submit' value='Greet me!'>" +
+          "</form>" +
+        "</body>" +
+      "</html>";
+  }
+
 }
